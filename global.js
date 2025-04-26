31cc468f1388114a8751fb8f55140b1dff5350eb
vermastutz <129792092+vermastutz@users.noreply.github.com>
2025-04-25 19:44:18 -0700

    lab4

--- global.js
+++ global.js
@@ -134,6 +134,6 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
 }
 
 export async function fetchGitHubData(username) {
-  return fetchJSON(`https://api.github.com/users/${vermastutz}`);
+  return fetchJSON(`https://api.github.com/users/${username}`);
 }
 
