17067399be76cd086d527731114d22b2517e7af3
vermastutz <129792092+vermastutz@users.noreply.github.com>
2025-04-25 18:25:16 -0700

    lab04

--- global.js
+++ global.js
@@ -105,3 +105,30 @@ form?.addEventListener('submit', function (event) {
   const mailtoURL = `${form.action}?${params.join('&')}`;
   location.href = mailtoURL;
 });
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = ''; // Clear existing content
+
+  for (const project of projects) {
+    const article = document.createElement('article');
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+    containerElement.appendChild(article);
+  }
+}
