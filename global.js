c40939416204686281ea04b6165f74a1f2a4c826
vermastutz <129792092+vermastutz@users.noreply.github.com>
2025-04-25 19:32:33 -0700

    lab4

--- global.js
+++ global.js
@@ -136,3 +136,4 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
 export async function fetchGitHubData(username) {
   return fetchJSON(`https://api.github.com/users/${vermastutz}`);
 }
+
--- index.js
+++ index.js
@@ -9,3 +9,17 @@ const projectsContainer = document.querySelector('.projects');
 if (projectsContainer) {
   renderProjects(latestProjects, projectsContainer, 'h2');
 }
+
+const githubData = await fetchGitHubData('vermastutz');
+const profileStats = document.querySelector('#profile-stats');
+
+if (profileStats) {
+  profileStats.innerHTML = `
+    <dl>
+      <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+      <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+      <dt>Followers:</dt><dd>${githubData.followers}</dd>
+      <dt>Following:</dt><dd>${githubData.following}</dd>
+    </dl>
+  `;
+}
