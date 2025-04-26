82d274475617c9bec6133cf53571b1434ce0466d
vermastutz <129792092+vermastutz@users.noreply.github.com>
2025-04-25 18:53:29 -0700

    lab4

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,11 @@
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
-
 const projectsContainer = document.querySelector('.projects');
+const title = document.querySelector('.projects-title');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+if (title) {
+  title.textContent = `Projects (${projects.length})`;
+}
