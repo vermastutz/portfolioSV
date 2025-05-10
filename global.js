fc98672fa7bca95ce40b34940d3c2c4b33a73b91
vermastutz <129792092+vermastutz@users.noreply.github.com>
2025-05-09 18:53:44 -0700

    test

--- global.js
+++ global.js
@@ -3,13 +3,7 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
-// let navLinks = $$("nav a");
 
-// let currentLink = navLinks.find(
-//   (a) => a.host === location.host && a.pathname === location.pathname
-// );
-
-// currentLink?.classList.add("current");
 
 
 let pages = [
